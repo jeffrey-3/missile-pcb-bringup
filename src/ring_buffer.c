8efe573e333488d51a2f9fad7f23bacc9145531f
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2025-12-31 16:52:47 -0500

    Fix indentation in ring_buffer

--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -12,33 +12,33 @@ bool ring_buffer_empty(ring_buffer_t* rb) {
 }
 
 bool ring_buffer_read(ring_buffer_t* rb, uint8_t* byte) {
-  uint32_t local_read_index = rb->read_index;
-  uint32_t local_write_index = rb->write_index;
+    uint32_t local_read_index = rb->read_index;
+    uint32_t local_write_index = rb->write_index;
 
-  if (local_read_index == local_write_index) {
-    return false;
-  }
+    if (local_read_index == local_write_index) {
+        return false;
+    }
 
-  *byte = rb->buffer[local_read_index];
-  local_read_index = (local_read_index + 1) & rb->mask;
-  rb->read_index = local_read_index;
+    *byte = rb->buffer[local_read_index];
+    local_read_index = (local_read_index + 1) & rb->mask;
+    rb->read_index = local_read_index;
 
-  return true;
+    return true;
 }
 
 bool ring_buffer_write(ring_buffer_t* rb, uint8_t byte) {
-  uint32_t local_write_index = rb->write_index;
-  uint32_t local_read_index = rb->read_index;
+    uint32_t local_write_index = rb->write_index;
+    uint32_t local_read_index = rb->read_index;
 
-  uint32_t next_write_index = (local_write_index + 1) & rb->mask;
+    uint32_t next_write_index = (local_write_index + 1) & rb->mask;
 
-  if (next_write_index == local_read_index) {
-    return false;
-  }
+    if (next_write_index == local_read_index) {
+        return false;
+    }
 
-  rb->buffer[local_write_index] = byte;
-  rb->write_index = next_write_index;
-  return true;
+    rb->buffer[local_write_index] = byte;
+    rb->write_index = next_write_index;
+    return true;
 }
 
 uint32_t ring_buffer_count(ring_buffer_t* rb) {
